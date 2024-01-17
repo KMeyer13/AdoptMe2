173ed66fab631ebaf1ff3ccb3a0708686b667141
KMeyer13 <meyerrkyle@aol.com>
2024-01-17 10:43:43 -0600

    Error Boundaries

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Details = () => {
   const { id } = useParams();
@@ -33,4 +34,11 @@ const Details = () => {
   );
 };
 
-export default Details;
+// export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
